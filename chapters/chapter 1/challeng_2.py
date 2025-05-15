f7154bdd79ac0f6af1cdcbb6b8916591d467cf88
macbriyan <KANGMACBRIYAN@GMAIL.COM>
2025-05-15 05:14:52 -0700

    challeng 2

---
+++ chapters/chapter 1/challeng_2.py
@@ -0,0 +1,4 @@
+season = input('season of the year:')
+number = input('enter a number:')
+adjective = input('enter a adjective:')
+print("on a ,"  + adjective +""  + season +"" + "day i drink at least," + number + "" + "cups of juice")
