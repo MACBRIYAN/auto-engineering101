40e9c4ab1b0475e7a7570e0b2105a4a87e77e1f7
macbriyan <KANGMACBRIYAN@GMAIL.COM>
2025-05-15 04:20:02 -0700

    challeng one

---
+++ chapters/chapter 1/first_program.py
@@ -0,0 +1,4 @@
+first_name = input('please enter your first name: ')
+# this is just to ask a user of  their names
+last_name = input('please enter your last name :')
+print( first_name +"" + last_name +"")
